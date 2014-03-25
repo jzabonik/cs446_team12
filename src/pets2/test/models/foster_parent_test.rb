850f3223135ee353968e399650ec2631416c1c1d
Israel Gomez <igomez@mymail.mines.edu>
2014-03-24 19:13:34 -0600

    Added test so the foster parent attributes are not empty

--- src/pets2/test/models/foster_parent_test.rb
+++ src/pets2/test/models/foster_parent_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class FosterParentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "foster parent attributes must not be empty" do
+		foster_parent = FosterParent.new
+		assert foster_parent.invalid?
+		assert foster_parent.errors[:name].any?
+		assert foster_parent.errors[:address].any?
+		assert foster_parent.errors[:email].any?
+	end
 end
